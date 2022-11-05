a73d345d29c0313a5a8e2aaa9e7eef87092a39ce
pradeepkarthik77 <pradeepkarthikm@gmail.com>
2022-11-06 00:51:49 +0530

    Read the data from CSV file and returned the matched values in a array back to NutritionActivity. Now we need to take these 2 arrays and store it in a recyclerview and display it to the user.

--- app/src/main/java/com/example/nutrition/NutritionActivity.java
+++ app/src/main/java/com/example/nutrition/NutritionActivity.java
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 import android.widget.Toast;
+import java.io.InputStreamReader;
 
 public class NutritionActivity extends AppCompatActivity
 {
 
     public String item_id;
     Intent intent;
+
+    public String[] nutrition_topics;
+    public String[] nutrition_values;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -22,7 +25,31 @@ public class NutritionActivity extends AppCompatActivity
         intent = getIntent();
         item_id = intent.getStringExtra("item_id");
 
-        
+        ReadNutritioncsv readNutritioncsv = new ReadNutritioncsv();
+
+        try {
+            InputStreamReader inputStreamReader = new InputStreamReader(getResources().openRawResource(R.raw.nutrition_table));
+            readNutritioncsv.setValues(inputStreamReader);
+        }
+        catch(Exception e)
+        {
+            Toast.makeText(getApplicationContext(),"Cannot find the Apt food",Toast.LENGTH_SHORT).show();
+            nutrition_topics = new String[]{};
+            //TODO Write code to return to the Mainactivity here
+        }
+        try {
+            nutrition_topics = readNutritioncsv.read_topics();
+            nutrition_values = readNutritioncsv.read_values(item_id);
+            if(nutrition_values.length == 0)
+            {
+                throw new Exception("not found error");
+            }
+        }
+        catch(Exception e)
+        {
+            Toast.makeText(getApplicationContext(),"Cannot find data",Toast.LENGTH_SHORT).show();
+            //TODO Write code to return to MainActivity
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/nutrition/ReadNutritioncsv.java
@@ -0,0 +1,52 @@
+package com.example.nutrition;
+import com.opencsv.CSVReader;
+import java.io.InputStreamReader;
+
+public class ReadNutritioncsv
+{
+    public String[] nutrition_topics;
+    public String[] nutrtion_data;
+    protected CSVReader csvReader;
+
+    public ReadNutritioncsv()
+    {
+        nutrition_topics = new String[]{};
+        nutrtion_data = new String[]{};
+    }
+
+    public void setValues(InputStreamReader inputStreamReader) throws Exception
+    {
+        csvReader = new CSVReader(inputStreamReader);
+    }
+
+    public String[] read_topics() throws Exception
+    {
+        String[] nextreader = new String[]{};
+        nextreader = csvReader.readNext();
+        if(nextreader !=null)
+        {
+            nutrition_topics = nextreader;
+            return nutrition_topics;
+        }
+        return null;
+    }
+
+    public String[] read_values(String item_id) throws Exception
+    {
+        String[] nextreader = new String[]{};
+
+        while((nextreader = csvReader.readNext()) != null)
+        {
+            if(nextreader != null)
+            {
+                if(nextreader[0].equals(item_id))
+                {
+                    nutrtion_data = nextreader;
+                    return nutrtion_data;
+                }
+            }
+        }
+
+        return new String[]{};
+    }
+}
