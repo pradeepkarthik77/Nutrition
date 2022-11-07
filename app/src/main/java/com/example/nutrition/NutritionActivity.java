4d4f47ce000cc145e4a31abc4229d7ee3b759087
pradeepkarthik77 <pradeepkarthikm@gmail.com>
2022-11-07 14:06:42 +0530

    Displays the data to the user but still is unable to write the data into the csv file. Other than that all work is Done!!!

--- app/src/main/java/com/example/nutrition/NutritionActivity.java
+++ app/src/main/java/com/example/nutrition/NutritionActivity.java
@@ -1,11 +1,21 @@
 package com.example.nutrition;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+import com.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileWriter;
 import java.io.InputStreamReader;
+import java.net.URI;
 
 public class NutritionActivity extends AppCompatActivity
 {
@@ -16,6 +26,13 @@ public class NutritionActivity extends AppCompatActivity
     public String[] nutrition_topics;
     public String[] nutrition_values;
 
+    public RecyclerView nutrition_recycler;
+    public NutritionRecyclerAdapter nutrition_recyclerAdapter;
+
+    public MaterialButton back_btn;
+
+    public MaterialButton save_data;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -24,6 +41,9 @@ public class NutritionActivity extends AppCompatActivity
 
         intent = getIntent();
         item_id = intent.getStringExtra("item_id");
+
+        back_btn = findViewById(R.id.backbutton);
+        save_data = findViewById(R.id.savebutton);
 
         ReadNutritioncsv readNutritioncsv = new ReadNutritioncsv();
 
@@ -35,7 +55,7 @@ public class NutritionActivity extends AppCompatActivity
         {
             Toast.makeText(getApplicationContext(),"Cannot find the Apt food",Toast.LENGTH_SHORT).show();
             nutrition_topics = new String[]{};
-            //TODO Write code to return to the Mainactivity here
+            finish();
         }
         try {
             nutrition_topics = readNutritioncsv.read_topics();
@@ -48,8 +68,50 @@ public class NutritionActivity extends AppCompatActivity
         catch(Exception e)
         {
             Toast.makeText(getApplicationContext(),"Cannot find data",Toast.LENGTH_SHORT).show();
-            //TODO Write code to return to MainActivity
+            finish();
         }
+
+        back_btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                finish();
+            }
+        });
+
+        nutrition_recycler = (RecyclerView) findViewById(R.id.nutrition_recycler);
+        nutrition_recyclerAdapter = new NutritionRecyclerAdapter();
+        nutrition_recyclerAdapter.setValues(nutrition_topics,nutrition_values,this);
+        nutrition_recycler.setAdapter(nutrition_recyclerAdapter);
+        nutrition_recycler.setLayoutManager(new LinearLayoutManager(this));
+
+        save_data.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                try
+                {
+                    String uristring = "file:///android_asset/user_nutrition.csv";
+                    URI uri = URI.create(uristring);
+                    File csvfile = new File(new URI("file:///android_asset/user_nutrition"));
+                    FileWriter fileWriter = new FileWriter(csvfile);
+                    CSVWriter csvWriter = new CSVWriter(fileWriter);
+                    String[] Writeit = nutrition_values;
+                    csvWriter.writeNext(new String[]{"101","10","11","12","13","14","15","11","12"});
+                    Toast.makeText(getApplicationContext(),"Data Saved",Toast.LENGTH_LONG);
+                    finish();
+                }
+                catch(Exception e)
+                {
+                    System.out.println("999999999999999999999999999999999999999999999999999999999999999999999999999999999");
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),"hi",Toast.LENGTH_LONG);
+                    //finish();
+                }
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/nutrition/NutritionRecyclerAdapter.java
+++ app/src/main/java/com/example/nutrition/NutritionRecyclerAdapter.java
@@ -1,35 +1,63 @@
 package com.example.nutrition;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.Arrays;
+
 public class NutritionRecyclerAdapter extends RecyclerView.Adapter<NutritionRecyclerAdapter.ViewHolder>
 {
+    public String[] nutrition_topics;
+    public String[] nutrition_values;
+    public Context context;
+
+    public void setValues(String[] nutrition_topics,String[] nutrition_values,Context context)
+    {
+        this.nutrition_topics = Arrays.copyOfRange(nutrition_topics,1,nutrition_topics.length+1);
+        this.nutrition_values = Arrays.copyOfRange(nutrition_values,1,nutrition_values.length+1);
+        this.context = context;
+    }
+
     public class ViewHolder extends RecyclerView.ViewHolder
     {
+        public TextView item_topic;
+        public TextView item_value;
+
         public ViewHolder(@NonNull View itemView)
         {
             super(itemView);
+            item_topic = itemView.findViewById(R.id.nutrition_topic);
+            item_value = itemView.findViewById(R.id.nutrition_value);
         }
     }
 
     @NonNull
     @Override
-    public NutritionRecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public NutritionRecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View newview = inflater.inflate(R.layout.nutritionrow,parent,false);
+
+        return new ViewHolder(newview);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull NutritionRecyclerAdapter.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull NutritionRecyclerAdapter.ViewHolder holder, int position)
+    {
+        holder.item_topic.setText(nutrition_topics[position]+": ");
+        holder.item_value.setText(nutrition_values[position]);
     }
 
     @Override
-    public int getItemCount() {
-        return 0;
+    public int getItemCount()
+    {
+        return this.nutrition_topics.length-1;
     }
 
 
